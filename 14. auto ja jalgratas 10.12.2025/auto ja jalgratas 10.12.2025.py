4941757a5eaf6adcff29aa9e52df8f4bf03701c1
stefanploom <stefan.ploom@opilane.hariduskeskus.ee>
2025-12-10 10:43:04 +0200

    Create auto ja jalgratas 10.12.2025.py

---
+++ 14. auto ja jalgratas 10.12.2025/auto ja jalgratas 10.12.2025.py
@@ -0,0 +1,8 @@
+class Soiduk
+    def __init__(self, mark: string, aasta: int, kiirus)
+    self.mark = mark
+    self.string = string
+    self.aasta = aasta
+    self.kiirus = kiirus
+    
+    def soidakm(self, labisoit)
